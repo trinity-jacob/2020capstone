4cdb1c34e287dd3804df14502b7cf988f7fc21df
Stephen Wright <stephen.wright@OUSD.ORG>
2020-03-07 08:38:08 -0800

    setup edits

--- app/__init__.py
+++ app/__init__.py
@@ -9,7 +9,8 @@ from flask_moment import Moment
 app = Flask(__name__)
 app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY") or os.urandom(20)
 # you must change the next line to be link to your database at mlab
-connect("<yourDBName>", host='mongodb://<user>:<password>@<somerandoNumber>.mlab.com:41643/<yourDBName>', retryWrites=False)
+connect("<yourDBName>", host='mongodb+srv://<YourUserName>:<YourPassword>@cluster0-8m0v1.gcp.mongodb.net/test?retryWrites=true&w=majority')
+
 moment = Moment(app)
 
 from .routes import *
